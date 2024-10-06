4119486d3d4f0ec901e99d6ccb487df386f9faea
DecentMakeover <Ryanloaded@live.com>
2024-10-06 15:18:51 +0530

    Update file_uploader.py

--- pages/file_uploader.py
+++ pages/file_uploader.py
@@ -1,12 +1,17 @@
 import streamlit as st
+import base64
 
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_file is not None:
-    # Save the uploaded file locally
+    # Save PDF to temp location
     with open("temp.pdf", "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # Display PDF using an iframe with file path
-    pdf_display = f'<iframe src="temp.pdf" width="700" height="800" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    # Encode PDF to base64
+    with open("temp.pdf", "rb") as pdf_file:
+        base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+
+    # Embed PDF within iframe
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="800" type="application/pdf"></iframe>'
+    st.components.v1.html(pdf_display, height=800)
